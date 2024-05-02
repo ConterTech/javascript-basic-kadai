aa52810dce9ad1773c527e8a36cf61ee4b3cf9ea
ConterTech <time6221@gmail.com>
2024-05-02 19:03:02 +0900

    kadai_020を提出

---
+++ event.js
@@ -0,0 +1,6 @@
+const changeBtn = document.getElementById('btn');
+
+changeBtn.addEventListener('click', () => {
+  const changeh1 = document.getElementById('text');
+  changeh1.innerHTML = 'ボタンをクリックしました';
+});
