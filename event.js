1bc23249ee285c52a3daefa61e70b632ba0d0640
ConterTech <164598095+ConterTech@users.noreply.github.com>
2024-05-02 21:40:31 +0900

    Delete event.js

--- event.js
+++
@@ -1,6 +0,0 @@
-const changeBtn = document.getElementById('btn');
-
-changeBtn.addEventListener('click', () => {
-  const changeh1 = document.getElementById('text');
-  changeh1.innerHTML = 'ボタンをクリックしました';
-});
